bf606f21d7b1b857526c9670eb2bf2f89c1ad402
Leonardo <leo_16-08@hotmail.com>
2019-08-04 19:46:03 -0300

    Inicio

---
+++ src/main/java/com/apicarrinhodecompra/ApiCarrinhoDePontaApplication.java
@@ -0,0 +1,13 @@
+package com.apicarrinhodecompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCarrinhoDePontaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCarrinhoDePontaApplication.class, args);
+	}
+
+}
